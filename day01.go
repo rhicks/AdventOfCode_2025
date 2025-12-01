b70ce91243cd58c885ee4f72220a5a5003509f83
Richard Hicks <rhicks@linkoregon.org>
2025-12-01 08:33:28 -0800

    day1_1 completed

---
+++ day01.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func day01_1(input string) {
+	currentPosition := 50
+	zeroHit := 0
+	number := 0
+	lines := strings.SplitSeq(strings.TrimSpace(input), "\n")
+
+	for line := range lines {
+		if strings.Contains(line, "R") {
+			// fmt.Println("ADD")
+			number, _ = strconv.Atoi(line[1:])
+			for number > 100 {
+				number = number - 100
+			}
+			currentPosition, zeroHit = getDelta(currentPosition, number, zeroHit)
+		} else if strings.Contains(line, "L") {
+			// .Println("SUBTRACT")
+			number, _ = strconv.Atoi(line[1:])
+			for number > 100 {
+				number = number - 100
+			}
+			number = number * -1
+			currentPosition, zeroHit = getDelta(currentPosition, number, zeroHit)
+		} else {
+			fmt.Println("ERROR")
+		}
+		fmt.Println(currentPosition)
+	}
+	fmt.Println(zeroHit)
+}
+
+func getDelta(current int, new int, hitCounter int) (int, int) {
+	delta := current + new
+
+	if delta == 100 || delta == 0 {
+		hitCounter++
+		return 0, hitCounter
+	}
+	if delta < 0 {
+		delta = 100 + delta
+	}
+	if delta > 100 {
+		delta = delta - 100
+	}
+
+	return delta, hitCounter
+
+}
---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func getInput(day int) string {
+	inputDir := "inputs"
+	inputFile := filepath.Join(inputDir, fmt.Sprintf("day%d.txt", day))
+
+	if _, err := os.Stat(inputFile); err == nil {
+		content, _ := os.ReadFile(inputFile)
+		return string(content)
+	}
+
+	return ""
+}
+
+func main() {
+	day := 1
+	input := getInput(day)
+
+	day01_1(input)
+}
